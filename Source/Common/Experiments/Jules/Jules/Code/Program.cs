cc60f893570828b9eb93f63f0dd023b97df6a9d5
MinexAutomation <minex.automation@gmail.com>
2017-05-08 18:00:03 -0400

    Jules - Experiment in use of the Math.NET library.

---
+++ Source/Common/Experiments/Jules/Jules/Code/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+using MathNet.Numerics.Distributions;
+using MathNet.Numerics.Statistics;
+
+
+namespace Public.Common.Jules
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random rand = new Random(0);
+            var samples = new ContinuousUniform(0, 1, rand).Samples().Take(1000);
+            var statistics = new DescriptiveStatistics(samples);
+
+            var mean = statistics.Mean;
+            var variance = statistics.Variance;
+            var skewness = statistics.Skewness;
+            var kurtosis = statistics.Kurtosis;
+        }
+    }
+}
