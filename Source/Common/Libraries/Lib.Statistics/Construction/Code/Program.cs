b5373f4dc3bf95ac0b888a71ab368028beaf697c
MinexAutomation <minex.automation@gmail.com>
2017-05-08 18:01:02 -0400

    Public.Common.Lib.Statistics - First commit of functionality to calculate basic statistics. In addition, I point to projects showing how to use Excel as a statistics calculator, and the Math.NET Numerics DescriptiveStatistics class as a statistics calculator.

---
+++ Source/Common/Libraries/Lib.Statistics/Construction/Code/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Public.Common.Lib.Statistics.Construction
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
