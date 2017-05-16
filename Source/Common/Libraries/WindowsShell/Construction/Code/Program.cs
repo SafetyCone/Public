b5edc570d43e4bab67acc8c839a2bfcab1686e57
MinexAutomation <minex.automation@gmail.com>
2017-05-16 14:21:00 -0400

    Public.Common.WindowsShell - Added VS versioned solutions using the new standard pattern.

---
+++ Source/Common/Libraries/WindowsShell/Construction/Code/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Public.Common.WindowsShell.Construction
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
