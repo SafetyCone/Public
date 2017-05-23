39050dd8b4832808018f0c4ad1bcc8360ce8095d
MinexAutomation <minex.automation@gmail.com>
2017-05-22 23:33:03 -0400

    NetCoreConsoleApp1 and NetCoreConsoleApp2 - Commit at a state allowing VS Code debugging.

--- Source/Experiments/Experiments/NetCoreConsoleApp1/src/NetCoreConsoleApp1/Code/Program.cs
+++ Source/Experiments/Experiments/NetCoreConsoleApp1/src/NetCoreConsoleApp1/Code/Program.cs
@@ -10,8 +10,9 @@ namespace NetCoreConsoleApp1
         public static void Main(string[] args)
         {
             UsefulClass useful = new UsefulClass();
-            useful.Value = @"Other value!";
+            Console.WriteLine(useful.Value);
 
+            useful.Value = @"Other value!";
             Console.WriteLine(useful.Value);
         }
     }
