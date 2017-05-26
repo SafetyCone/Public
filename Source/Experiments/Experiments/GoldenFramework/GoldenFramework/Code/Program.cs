2cd87fa63d03be35bcca0a4b6725dd1fd92d0ad6
MinexAutomation <minex.automation@gmail.com>
2017-05-26 00:07:58 -0400

    Public-Experiments-Golden-Framework - A classic .NET framework application that uses a .NET Standard library. Magic versions seem to be NET Standard 1.4 and NET Framework 4.6.1.

---
+++ Source/Experiments/Experiments/GoldenFramework/GoldenFramework/Code/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Library;
+
+
+namespace GoldenFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The answer is {new Thing().Get(42)}.");
+        }
+    }
+}
