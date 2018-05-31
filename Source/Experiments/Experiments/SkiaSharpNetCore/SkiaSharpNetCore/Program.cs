354186ffbf64beb803e1bb14de2aa136f8409569
MinexAutomation <minex.automation@gmail.com>
2018-05-31 07:35:55 -0400

    SkiaSharp - initial commit of first SkiaSharp .NET Core experiment in using the library to read images.

---
+++ Source/Experiments/Experiments/SkiaSharpNetCore/SkiaSharpNetCore/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+using SkiaSharp;
+
+
+namespace SkiaSharpNetCore
+{
+    class Program
+    {
+        private static string GetFileRootedPath()
+        {
+            string directoryRootedPath = @"C:\Users\David\Downloads";
+            string fileName = @"20180303_151739.jpg";
+
+            string fileRootedPath = Path.Combine(directoryRootedPath, fileName);
+            return fileRootedPath;
+        }
+
+        static void Main(string[] args)
+        {
+            string fileRootedPath = Program.GetFileRootedPath();
+
+            Console.WriteLine($"Image file path: {fileRootedPath}");
+
+            using (var input = File.OpenRead(fileRootedPath))
+            {
+                using (var inputStream = new SKManagedStream(input))
+                {
+                    using (var image = SKBitmap.Decode(inputStream))
+                    {
+                        Console.WriteLine($"Height: {image.Height.ToString()}");
+                    }
+                }
+            }
+        }
+    }
+}
