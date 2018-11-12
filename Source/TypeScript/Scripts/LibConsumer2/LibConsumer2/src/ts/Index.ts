f209f1f5c51fe281ccc26a9ba38260f304df46aa
MinexAutomation <minex.automation@gmail.com>
2018-11-11 22:21:09 -0800

    TypeScript - Changed SVN to ignore "dist" folders, removing the need for any SVN directory properties.

--- Source/TypeScript/Scripts/LibConsumer2/LibConsumer2/src/ts/Index.ts
+++ Source/TypeScript/Scripts/LibConsumer2/LibConsumer2/src/ts/Index.ts
@@ -1,4 +1,18 @@
 import * as TL2 from "@rivetanomalytics/testlib2"
+// import "../../node_modules/@rivetanomalytics/common/dist/Extensions/Array";
+// import "@rivetanomalytics/common/dist/Extensions/Array"
+import "@rivetanomalytics/common"
+
+// import { SayHello } from "@rivetanomalytics/common"
 
 TL2.sayHello();
 TL2.sayGoodbye();
+
+let numberArray: number[] = [1, 2, 3, -1];
+let min = numberArray.minimum();
+console.log(min);
+
+// SayHello();
+
+// let stringArray: string[];
+// stringArray.min
