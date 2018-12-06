44709105c58e15e6ae6bc8784c9e43f277f5c9ba
MinexAutomation <minex.automation@gmail.com>
2018-12-06 16:11:12 -0500

    Scratch - Created a scratchwork console-app for experiments and investigations related to .NET and C#.

---
+++ Source/Experiments/Experiments/Scratch/Scratch/Code/Enumerations/TestEnumeration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+public enum TestEnumeration
+{
+	// Static methods in an enumeration are not allowed.
+//	public static void Test()
+//{
+
+//}
+
+	Default,
+	Value1,
+	Value2
+}
