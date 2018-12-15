e949e7e0005304770c41f1a8cd1837f292ba00ee
MinexAutomation <minex.automation@gmail.com>
2018-12-15 14:43:56 -0500

    Add a quick example of a TypeScript lib and lib consumer.

---
+++ Source/Experiments/TypeScript/davidlib/src/hello-world.ts
@@ -0,0 +1,7 @@
+export function sayHello() {
+    console.log('hi');
+    console.log('Hello!');
+}
+export function sayGoodbye() {
+    console.log('goodbye');
+}
---
+++ Source/Experiments/TypeScript/davidlibconsumer/src/ts/Main.ts
@@ -0,0 +1,4 @@
+import { sayHello, sayGoodbye } from "davidlib";
+
+sayHello();
+sayGoodbye();  
