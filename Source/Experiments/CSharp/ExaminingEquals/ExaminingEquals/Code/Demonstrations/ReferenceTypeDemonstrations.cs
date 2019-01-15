0894fc01793c8994f80c231e97e17910e41648ee
MinexAutomation <minex.automation@gmail.com>
2019-01-15 10:50:44 -0500

    ExaminingEquals - Initial commit of console-app experiments examining and demonstrating concepts related to equality in C#.

---
+++ Source/Experiments/CSharp/ExaminingEquals/ExaminingEquals/Code/Demonstrations/ReferenceTypeDemonstrations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace ExaminingEquals
+{
+    public static class ReferenceTypeDemonstrations
+    {
+        public static void SubMain()
+        {
+            ReferenceTypeDemonstrations.ObjectEqualsReferenceEquality();
+            ReferenceTypeDemonstrations.ObjectEqualsSameAsObjectReferenceEquals();
+        }
+
+        private static void ObjectEqualsSameAsObjectReferenceEquals()
+        {
+
+        }
+
+        /// <summary>
+        /// Result: True.
+        /// Test whether two references to the same reference-type instance are Object.Equals().
+        /// </summary>
+        private static void ObjectEqualsReferenceEquality()
+        {
+            object a1 = new object();
+            object a2 = a1;
+
+            var a2EqualsA1 = a2.Equals(a1);
+            Console.WriteLine($@"Object a2.Equals(a1)? {a2EqualsA1}");
+        }
+    }
+}
---
+++ Source/Experiments/CSharp/ExaminingEquals/ExaminingEquals/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExaminingEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReferenceTypeDemonstrations.SubMain();
+        }
+    }
+}
