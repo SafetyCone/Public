a65e09503ecae15d067b25592091431098b2acbf
MinexAutomation <minex.automation@gmail.com>
2019-01-15 11:28:51 -0500

    ExaminingEquals - Added demonstration of reference-type Object.Equals() inequality. Renamed demonstraction of Object.Equals() equality.

--- Source/Experiments/CSharp/ExaminingEquals/ExaminingEquals/Code/Demonstrations/ReferenceTypeDemonstrations.cs
+++ Source/Experiments/CSharp/ExaminingEquals/ExaminingEquals/Code/Demonstrations/ReferenceTypeDemonstrations.cs
@@ -7,8 +7,10 @@ namespace ExaminingEquals
     {
         public static void SubMain()
         {
-            ReferenceTypeDemonstrations.ObjectEqualsReferenceEquality();
-            ReferenceTypeDemonstrations.ObjectEqualsSameAsObjectReferenceEquals();
+            //ReferenceTypeDemonstrations.ObjectEqualsSameInstanceReferenceEquality();
+            ReferenceTypeDemonstrations.ObjectEqualsDifferentInstanceReferenceInequality();
+
+            //ReferenceTypeDemonstrations.ObjectEqualsSameAsObjectReferenceEquals();
         }
 
         private static void ObjectEqualsSameAsObjectReferenceEquals()
@@ -17,13 +19,34 @@ namespace ExaminingEquals
         }
 
         /// <summary>
-        /// Result: True.
-        /// Test whether two references to the same reference-type instance are Object.Equals().
+        /// Result:
+        /// Test whether two reference-type variables that reference different instances are Object.Equals().
+        /// Expected: False, because reference-types use reference-equality by default.
+        /// 
+        /// Statement of reference-types using reference-equality for implementation of Object.Equals(): https://docs.microsoft.com/en-us/dotnet/api/system.object.equals?view=netframework-4.7.2#System_Object_Equals_System_Object_
         /// </summary>
-        private static void ObjectEqualsReferenceEquality()
+        private static void ObjectEqualsDifferentInstanceReferenceInequality()
         {
-            object a1 = new object();
-            object a2 = a1;
+            // Note, two constructor calls via 'new'.
+            var a1 = new object();
+            var a2 = new object();
+
+            var a2EqualsA1 = a2.Equals(a1);
+            Console.WriteLine($@"Object a2.Equals(a1)? {a2EqualsA1}");
+        }
+
+        /// <summary>
+        /// Result: True.
+        /// Test whether two reference-type variables that reference the same instance are Object.Equals().
+        /// Expected: True, because reference-types use reference-equality by default.
+        /// 
+        /// Statement of reference-types using reference-equality for implementation of Object.Equals(): https://docs.microsoft.com/en-us/dotnet/api/system.object.equals?view=netframework-4.7.2#System_Object_Equals_System_Object_
+        /// </summary>
+        private static void ObjectEqualsSameInstanceReferenceEquality()
+        {
+            // Note, one constructor call via 'new', and one assignment.
+            var a1 = new object();
+            var a2 = a1;
 
             var a2EqualsA1 = a2.Equals(a1);
             Console.WriteLine($@"Object a2.Equals(a1)? {a2EqualsA1}");
