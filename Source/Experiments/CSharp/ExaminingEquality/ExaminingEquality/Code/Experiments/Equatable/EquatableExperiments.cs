75ba18feea361205cf9c851df2a789c3995f0bc7
MinexAutomation <minex.automation@gmail.com>
2019-01-21 15:45:53 -0500

    ExaminingEquality - Added a question about IEquatable.

---
+++ Source/Experiments/CSharp/ExaminingEquality/ExaminingEquality/Code/Experiments/Equatable/EquatableExperiments.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ExaminingEquality
+{
+    public static class EquatableExperiments
+    {
+        public static void SubMain()
+        {
+            // How does other.GetType() != this.GetType() handle inheritance in IEquatable.Equals()?
+        }
+    }
+}
