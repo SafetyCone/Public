683b1181352c93b0cc070859ef79c637cd1b9622
MinexAutomation <minex.automation@gmail.com>
2019-01-27 16:40:15 -0500

    ExaminingCSharp - Initial commit of an experimental console-app for examining concepts in C# that are either general, uncategorized, or not-yet categorized.

---
+++ Source/Experiments/CSharp/ExaminingCSharp/ExaminingCSharp/Code/Experiments.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace ExaminingCSharp
+{
+    public static class Experiments
+    {
+        public static void SubMain()
+        {
+            Experiments.CanSetInDictionary();
+        }
+
+        /// <summary>
+        /// Result: False! You CAN set the value of a key that has not yet been added!
+        /// Does the System dictionary allow setting values for keys that have not yet been added?
+        /// Expected: Attempting to set the value of a not-yet added key will throw an error.
+        /// 
+        /// 
+        /// </summary>
+        private static void CanSetInDictionary()
+        {
+            var dictionary = new Dictionary<string, int>();
+
+            foreach (var key in dictionary.Keys)
+            {
+                // Do nothing, this is just to get over the initialization.
+            }
+
+            dictionary[@"one"] = 1;
+        }
+    }
+}
---
+++ Source/Experiments/CSharp/ExaminingCSharp/ExaminingCSharp/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExaminingCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Experiments.SubMain();
+        }
+    }
+}
