2b4639ebac763d965f65207c54feef83844dd813
MinexAutomation <minex.automation@gmail.com>
2019-01-29 12:17:25 -0500

    ExaminingSynchronicity - Initial commit of experimental console-app investigating synchronicity (sync/async) in C#.

---
+++ Source/Experiments/CSharp/ExaminingSynchronicity/ExaminingSynchronicity/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExaminingSynchronicity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
