546d1f17575646b08fd925fbcd53ceba9a688da4
MinexAutomation <minex.automation@gmail.com>
2019-01-29 13:11:13 -0500

    ExaminingThreading - Initial commit of a console-app investigating threading in C#.

---
+++ Source/Experiments/CSharp/ExaminingThreading/ExaminingThreading/Code/Experiments.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace ExaminingThreading
+{
+    public static class Experiments
+    {
+        public static void SubMain()
+        {
+
+        }
+    }
+}
---
+++ Source/Experiments/CSharp/ExaminingThreading/ExaminingThreading/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExaminingThreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
