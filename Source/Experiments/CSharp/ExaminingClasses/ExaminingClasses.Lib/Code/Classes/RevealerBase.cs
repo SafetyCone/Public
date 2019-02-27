972690624cf09696938ebaa744a429e9b260c34f
MinexAutomation <minex.automation@gmail.com>
2019-02-27 17:56:15 -0500

    ExaminingClasses - Added an experiment that shows that protected property values of instances of base-classes are inaccessible from instances of derived-classes.

---
+++ Source/Experiments/CSharp/ExaminingClasses/ExaminingClasses.Lib/Code/Classes/RevealerBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace ExaminingClasses.Lib
+{
+    /// <summary>
+    /// The Revealer experiment tests whether a protected property value of an instances of a base-class can be revealed by a derived class that takes in an instance of the base class.
+    /// </summary>
+    public class RevealerBase
+    {
+        protected int Value { get; set; }
+
+
+        public RevealerBase()
+        {
+        }
+
+        public RevealerBase(int value)
+        {
+            this.Value = value;
+        }
+    }
+}
---
+++ Source/Experiments/CSharp/ExaminingClasses/ExaminingClasses.Lib/Code/Classes/RevealerDerived.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace ExaminingClasses.Lib
+{
+    /// <summary>
+    /// Result: UNEXPECTED! Instances of a derived class are treated just like any other member of the public when it comes to being able to access protected properties.
+    /// The Revealer experiments tests whether a protected property value of a base class instance can be revealed by a derived class.
+    /// Expected: Yes, protected properties of 
+    /// </summary>
+    public class RevealerDerived : RevealerBase
+    {
+        public RevealerDerived()
+        {
+        }
+
+        public RevealerDerived(int value)
+            : base(value)
+        {
+        }
+
+        public RevealerDerived(RevealerBase baseInstance)
+            //: this(baseInstance.va)
+        {
+            //baseInstance.va   
+        }
+
+        //public int GetValue(RevealerBase revealerBase)
+        //{
+        //    var output = revealerBase.valu
+        //}
+    }
+}
