85072267a1f6b2607c0089d05577f9d0036848c3
MinexAutomation <minex.automation@gmail.com>
2019-03-28 22:14:49 -0400

    ExaminingOptions - Added examination of TryAddEnumerable().

--- Source/Experiments/CSharp/ExaminingOptions/ExaminingOptions.Lib/Code/Classes/ConsoleWriterExtraConfigurator.cs
+++ Source/Experiments/CSharp/ExaminingOptions/ExaminingOptions.Lib/Code/Classes/ConsoleWriterExtraConfigurator.cs
@@ -5,11 +5,11 @@ using Microsoft.Extensions.Options;
 
 namespace ExaminingOptions.Lib
 {
-    public class ConsoleWriterConfigurator : IConfigureOptions<ConsoleWriterOptions>
+    public class ConsoleWriterExtraConfigurator : IConfigureOptions<ConsoleWriterOptions>
     {
         public void Configure(ConsoleWriterOptions options)
         {
-            options.Message = $@"Message from {nameof(ConsoleWriterConfigurator)}.";
+            options.Message += @" YO!";
         }
     }
 }
--- Source/Experiments/CSharp/ExaminingOptions/ExaminingOptions/Code/Explorations.cs
+++ Source/Experiments/CSharp/ExaminingOptions/ExaminingOptions/Code/Explorations.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.IO;
 
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
 
+using ExaminingDependencyInjection.Lib;
 using ExaminingOptions.Lib;
 
 
@@ -14,9 +18,94 @@ namespace ExaminingOptions
         {
             //Explorations.ConsoleWriterOptionsManualConfiguration();
             //Explorations.ConsoleWriterAppSettingsConfiguration();
-            Explorations.ConsoleWriterConfiguratorConfiguration();
+            //Explorations.ConsoleWriterConfiguratorConfiguration();
+            //Explorations.ConsoleWriterTryAddEnumerableConfigurator();
+            Explorations.AddDuplicateServices();
+            //Explorations.AddDuplicateServicesWithTryAddEnumerable();
         }
 
+        /// <summary>
+        /// The TryAddEnumerable() method prevents the same implementation of the same service from being added multiple times.
+        /// </summary>
+        private static void AddDuplicateServicesWithTryAddEnumerable()
+        {
+            var services = new ServiceCollection()
+                   ;
+
+            services
+                   .TryAddEnumerable(ServiceDescriptor.Singleton<IConfigureOptions<ConsoleWriterOptions>, ConsoleWriterConfigurator>())
+                   ;
+
+            services
+                   .TryAddEnumerable(ServiceDescriptor.Singleton<IConfigureOptions<ConsoleWriterOptions>, ConsoleWriterExtraConfigurator>())
+                   ;
+
+            services
+                   .TryAddEnumerable(ServiceDescriptor.Singleton<IConfigureOptions<ConsoleWriterOptions>, ConsoleWriterConfigurator>())
+                   ;
+
+            services
+                   .TryAddEnumerable(ServiceDescriptor.Singleton<IConfigureOptions<ConsoleWriterOptions>, ConsoleWriterExtraConfigurator>())
+                   ;
+
+            var outputFilePath = @"C:\Temp\Services.txt";
+            using (var streamWriter = new StreamWriter(outputFilePath))
+            {
+                services.DescribeServices(streamWriter);
+            }
+        }
+
+        /// <summary>
+        /// There is nothing stopping users from adding the same implementation of the same service multiple times.
+        /// </summary>
+        private static void AddDuplicateServices()
+        {
+            var services = new ServiceCollection
+            {
+                ServiceDescriptor.Singleton<IConfigureOptions<ConsoleWriterOptions>, ConsoleWriterConfigurator>(),
+
+                ServiceDescriptor.Singleton<IConfigureOptions<ConsoleWriterOptions>, ConsoleWriterExtraConfigurator>(),
+
+                ServiceDescriptor.Singleton<IConfigureOptions<ConsoleWriterOptions>, ConsoleWriterConfigurator>(),
+
+                ServiceDescriptor.Singleton<IConfigureOptions<ConsoleWriterOptions>, ConsoleWriterExtraConfigurator>()
+            };
+
+            var outputFilePath = @"C:\Temp\Services.txt";
+            using (var streamWriter = new StreamWriter(outputFilePath))
+            {
+                services.DescribeServices(streamWriter);
+            }
+        }
+
+        /// <summary>
+        /// Use multiple other services to configure the <see cref="ConsoleWriterOptions"/>.
+        /// </summary>
+        private static void ConsoleWriterTryAddEnumerableConfigurator()
+        {
+            var services = new ServiceCollection()
+                   .AddOptions()
+                   .AddConsoleWriter()
+                   ;
+
+            services
+                   .TryAddEnumerable(ServiceDescriptor.Singleton<IConfigureOptions<ConsoleWriterOptions>, ConsoleWriterConfigurator>())
+                   ;
+
+            services
+                   .TryAddEnumerable(ServiceDescriptor.Singleton<IConfigureOptions<ConsoleWriterOptions>, ConsoleWriterExtraConfigurator>())
+                   ;
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var consoleWriter = serviceProvider.GetRequiredService<IConsoleWriter>();
+
+            consoleWriter.Write();
+        }
+
+        /// <summary>
+        /// Use another service to configure the <see cref="ConsoleWriterOptions"/>.
+        /// </summary>
         private static void ConsoleWriterConfiguratorConfiguration()
         {
             var services = new ServiceCollection()
@@ -33,6 +122,9 @@ namespace ExaminingOptions
             consoleWriter.Write();
         }
 
+        /// <summary>
+        /// Uses a configuration (built from an appsettings.json file) to configure the <see cref="ConsoleWriterOptions"/>.
+        /// </summary>
         private static void ConsoleWriterAppSettingsConfiguration()
         {
             var configurationBuilder = new ConfigurationBuilder()
@@ -56,6 +148,9 @@ namespace ExaminingOptions
             consoleWriter.Write();
         }
 
+        /// <summary>
+        /// Uses a manually code factory method to configure the <see cref="ConsoleWriterOptions"/>.
+        /// </summary>
         private static void ConsoleWriterOptionsManualConfiguration()
         {
             var services = new ServiceCollection()
