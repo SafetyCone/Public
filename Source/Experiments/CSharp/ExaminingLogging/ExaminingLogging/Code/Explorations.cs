ca3c020c976009b0bb227bafc9fdebfa88c548fa
MinexAutomation <minex.automation@gmail.com>
2019-03-28 21:53:36 -0400

    ExaminingLogging - Added exploration of getting a logger from DI, and getting the logger filter options added by the default call to AddLogging().

--- Source/Experiments/CSharp/ExaminingLogging/ExaminingLogging/Code/Explorations.cs
+++ Source/Experiments/CSharp/ExaminingLogging/ExaminingLogging/Code/Explorations.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 using ExaminingLogging.Lib;
 
@@ -11,7 +13,38 @@ namespace ExaminingLogging
     {
         public static void SubMain()
         {
-            Explorations.GetLoggerDirectly();
+            //Explorations.GetLoggerDirectly();
+            Explorations.GetLoggerFromDI();
+            //Explorations.GetLoggerFilterOptions();
+        }
+
+        private static void GetLoggerFilterOptions()
+        {
+            var services = new ServiceCollection();
+
+            services.AddLogging();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var loggerFilterOptions = serviceProvider.GetRequiredService<IOptions<LoggerFilterOptions>>();
+        }
+
+        private static void GetLoggerFromDI()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<ILoggerFactory, LoggerFactory>();
+            services.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+            loggerFactory.AddProvider(new ExtremelySimpleLoggerProvider());
+
+            var logger = loggerFactory.CreateLogger<Explorations>();
+
+            logger.LogInformation(@"Hello world!", 1, 3.3, DateTime.UtcNow);
         }
 
         /// <summary>
