3d79ef76db4181d2c6379a65fd23767234ef0181
MinexAutomation <minex.automation@gmail.com>
2019-04-01 22:50:46 -0400

    ExaminingConfiguration - Initial commit of exploration of configuration in C#.

---
+++ Source/Experiments/CSharp/ExaminingConfiguration/ExaminingConfiguration.Lib/Code/Extensions/IConfigurationSectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+
+namespace ExaminingConfiguration.Lib
+{
+    public static class IConfigurationSectionExtensions
+    {
+        public static void DescribeUsing(this IConfigurationSection configurationSection, Action<string> descriptionSink)
+        {
+            descriptionSink($@"{configurationSection.Path}: {configurationSection.Value}");
+        }
+
+        public static void DescibeConfiguration(this IEnumerable<IConfigurationSection> configurationSections, Action<string> descriptionSink)
+        {
+            foreach (var configurationSection in configurationSections)
+            {
+                configurationSection.DescribeUsing(descriptionSink);
+            }
+        }
+
+        public static void DescribeServices(this IEnumerable<IConfigurationSection> services, StreamWriter writer)
+        {
+            services.DescibeConfiguration(x => writer.WriteLine(x));
+        }
+
+        public static void DescribeServices(this IEnumerable<IConfigurationSection> services, ILogger logger)
+        {
+            services.DescibeConfiguration(x => logger.LogInformation(x));
+        }
+    }
+}
