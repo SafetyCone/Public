8afbfebcf6ceed283f182407a8c04a051e389900
MinexAutomation <minex.automation@gmail.com>
2019-04-14 12:12:26 -0400

    ExaminingEquality - Added OverrideEqualsClass example showing how to implement an override equals. Added stub for EquatableClass.

---
+++ Source/Experiments/CSharp/ExaminingEquality/ExaminingEquality/Code/Examples/Classes/EquatableClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExaminingEquality.Examples
+{
+    class EquatableClass
+    {
+    }
+}
---
+++ Source/Experiments/CSharp/ExaminingEquality/ExaminingEquality/Code/Examples/Classes/OverrideEqualsClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace ExaminingEquality.Examples
+{
+    /// <summary>
+    /// Provides the standard example of overriding equals.
+    /// Note that overriding <see cref="Object.Equals(object)"/> means you must override <see cref="Object.GetHashCode"/>.
+    /// 
+    /// Note also, that if you override <see cref="Object.Equals(object)"/>, you should probably implement <see cref="IEquatable{T}"/> (see <see cref="EquatableClass"/>).
+    /// </summary>
+    public class OverrideEqualsClass
+    {
+        public string Value { get; set; }
+
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !obj.GetType().Equals(this.GetType()))
+            {
+                return false;
+            }
+
+            var objAsType = obj as OverrideEqualsClass;
+
+            var isEqual = objAsType.Value.Equals(this.Value);
+            return isEqual;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = this.Value.GetHashCode();
+            return hashCode;
+        }
+    }
+}
