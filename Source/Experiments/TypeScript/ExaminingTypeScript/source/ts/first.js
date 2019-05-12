eb415119b0c1fd769cac53b11058f0aed17a59a8
MinexAutomation <minex.automation@gmail.com>
2019-05-12 00:07:17 -0400

    Adding nullCheck.

--- Source/Experiments/TypeScript/ExaminingTypeScript/source/ts/first.js
+++ Source/Experiments/TypeScript/ExaminingTypeScript/source/ts/first.js
@@ -1,2 +1,11 @@
-console.log("Hello!");
+let parameters;
+if (!parameters) {
+    console.log("!parameters is true");
+}
+if (parameters == undefined) {
+    console.log("parameters == undefined is true");
+}
+if (parameters == null) {
+    console.log("parameters == null is true");
+}
 //# sourceMappingURL=first.js.map
