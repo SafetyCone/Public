8680e323a00540074eade28deecd648ba4fcdb58
MinexAutomation <minex.automation@gmail.com>
2019-05-12 00:07:43 -0400

    Actually adding nullCheck.

---
+++ Source/Experiments/TypeScript/ExaminingTypeScript/source/ts/nullCheck.js
@@ -0,0 +1,33 @@
+let parameters;
+console.log("let parameters: Array<string>;");
+if (!parameters) {
+    console.log("!parameters is true");
+}
+else {
+    console.log("!parameters is FALSE");
+}
+if (parameters == undefined) {
+    console.log("parameters == undefined is true");
+}
+else {
+    console.log("parameters == undefined is FALSE");
+}
+if (parameters == null) {
+    console.log("parameters == null is true");
+}
+else {
+    console.log("parameters == null is FALSE");
+}
+if (parameters === undefined) {
+    console.log("parameters === undefined is true");
+}
+else {
+    console.log("parameters === undefined is FALSE");
+}
+if (parameters === null) {
+    console.log("parameters === null is true");
+}
+else {
+    console.log("parameters === null is FALSE");
+}
+//# sourceMappingURL=nullCheck.js.map
---
+++ Source/Experiments/TypeScript/ExaminingTypeScript/source/ts/nullCheck.ts
@@ -0,0 +1,32 @@
+let parameters: Array<string>;
+console.log("let parameters: Array<string>;");
+
+if(!parameters) {
+    console.log("!parameters is true");
+} else {
+    console.log("!parameters is FALSE");
+}
+
+if(parameters == undefined) {
+    console.log("parameters == undefined is true");
+} else {
+    console.log("parameters == undefined is FALSE");
+}
+
+if(parameters == null) {
+    console.log("parameters == null is true");
+} else {
+    console.log("parameters == null is FALSE");
+}
+
+if(parameters === undefined) {
+    console.log("parameters === undefined is true");
+} else {
+    console.log("parameters === undefined is FALSE");
+}
+
+if(parameters === null) {
+    console.log("parameters === null is true");
+} else {
+    console.log("parameters === null is FALSE");
+}
