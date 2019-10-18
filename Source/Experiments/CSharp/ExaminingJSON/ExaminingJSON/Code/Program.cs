871e7b0ccf1ce75a37fb24cf8d8da092e5cc9e34
MinexAutomation <minex.automation@gmail.com>
2019-10-18 09:31:14 -0700

    Initial commit of source-stub.

---
+++ Source/Experiments/CSharp/ExaminingJSON/ExaminingJSON/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExaminingJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
